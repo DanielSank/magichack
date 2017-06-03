01bf7e49a865806eaeac407ef07e493ad2115d83
Daniel Sank <sank.daniel@gmail.com>
2017-06-02 20:57:44 -0700

    Improve uploader.

--- magichack/upload.py
+++ magichack/upload.py
@@ -7,16 +7,19 @@ import magichack.models as models
 
 def get_cards_from_sheet(sheet_id, tab):
     sheet = gsheets.SingleSheet(sheet_id)
-    data = sheet.read_range_as_DataFrame('{}!A1:E14'.format(tab))
+    data = sheet.read_range_as_DataFrame('{}!A1:E100'.format(tab))
     cards = []
     for _, row in data.iterrows():
         major, minors = parse_type(row.TYPE)
         secondary_types = [models.SecondaryType(name=minor)
                            for minor in minors]
+        rule_texts = row.RULES.split(';')
+        rules = [models.Rule(text=rule_text) for rule_text in rule_texts]
         cards.append(models.Card(
                 name=row.NAME,
                 primary_type=major,
-                secondary_types=secondary_types))
+                secondary_types=secondary_types,
+                rules=rules))
     return cards
 
 
