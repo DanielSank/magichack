9dee82910d013fd369cfb618b23bfda98ae1d6f3
Daniel Sank <sank.daniel@gmail.com>
2026-08-05 01:02:12 -0700

    parse_gsheet returns list instead of dict.
    We can have multiple cards with the same name, e.g. basic lands,
    so we can't key by card name.
    
    There may be a larger issue to consider here, namely what sheet
    layout (schema) makes sense in light of the fact that we can have
    multiple cards with the same name, presumably the same rules text,
    but different images.

--- python/setparse.py
+++ python/setparse.py
@@ -6,7 +6,8 @@ from __future__ import annotations
 import os
 import csv
 
-from core import Card, Rarity, Cost
+import core
+from core import Rarity, Cost
 
 
 def parse_pt(pt: str) -> int | str | None:
@@ -22,7 +23,7 @@ def parse_pt(pt: str) -> int | str | None:
 def parse_gsheet(
         rows: list[list[str]],
         setcode: str,
-) -> dict[str, Card]:
+) -> list[core.Card]:
     # Format the gsheet rows a bit before creating Cards.
     column_names = rows[0]
     formatted: list[dict[str, str | list[str]]] = []
@@ -38,28 +39,30 @@ def parse_gsheet(
         d["Type"] = "\n".join(types)
         formatted.append(d)
 
-    cards: dict[str, Card] = {}
+    cards: list[core.Card] = []
     for row in formatted:
-        cards[row["Name"]] = Card(
-                sset=setcode,
-                rarity=Rarity.from_string(row["Rarity"]),
-                legendary=True if row["Legendary"] == "TRUE" else False,
-                types=tuple(row["Type"].split("\n")),
-                subtypes=tuple(row["Subtypes"].split("\n")),
-                classes=tuple(row["Classes"].split("\n")),
-                power=parse_pt(row["P"]),
-                toughness=parse_pt(row["T"]),
-                cost=Cost.from_str(row["Cost"]),
-                rules=tuple(row["Rules"].split("\n")),
-                name=row["Name"],
-                flavor=row.get("Flavor", ""),
-                image_url=row["Image"] if row.get("Image", None) else None,
+        cards.append(
+                core.Card(
+                    sset=setcode,
+                    rarity=Rarity.from_string(row["Rarity"]),
+                    legendary=True if row["Legendary"] == "TRUE" else False,
+                    types=tuple(row["Type"].split("\n")),
+                    subtypes=tuple(row["Subtypes"].split("\n")),
+                    classes=tuple(row["Classes"].split("\n")),
+                    power=parse_pt(row["P"]),
+                    toughness=parse_pt(row["T"]),
+                    cost=Cost.from_str(row["Cost"]),
+                    rules=tuple(row["Rules"].split("\n")),
+                    name=row["Name"],
+                    flavor=row.get("Flavor", ""),
+                    image_url=row["Image"] if row.get("Image", None) else None,
+                )
         )
     return cards
 
 
-def parse_gsheet_csv(filename: str, setcode: str) -> dict[str, Card]:
-    cards: dict[str, Card] = {}
+def parse_gsheet_csv(filename: str, setcode: str) -> dict[str, core.Card]:
+    cards: dict[str, core.Card] = {}
     with open(filename, mode="r", encoding="utf-8") as file:
         reader = csv.reader(file)
         _ = next(reader)  # Skip the header
@@ -67,7 +70,7 @@ def parse_gsheet_csv(filename: str, setcode: str) -> dict[str, Card]:
             rarity, legendary, ttype, _, subtypes, classes, p, t, cost, rules, name, image, flavor, *rest = tuple(row)
             if name == "":  # Skip empty sheet rows
                 continue
-            cards[name] = Card(
+            cards[name] = core.Card(
                     sset=setcode,
                     rarity=Rarity.from_string(rarity),
                     legendary=True if legendary == "TRUE" else False,
@@ -84,7 +87,7 @@ def parse_gsheet_csv(filename: str, setcode: str) -> dict[str, Card]:
     return cards
 
 
-def all_cards(set_tag: str) -> dict[str, Card]:
+def all_cards(set_tag: str) -> dict[str, core.Card]:
     """
     Args:
         set_tag: The three letter abbreviation for the set, e.g. "LEA"
@@ -92,7 +95,7 @@ def all_cards(set_tag: str) -> dict[str, Card]:
     """
     files = [f for f in os.listdir(".") if os.path.isfile(f)]
     files = [f for f in files if f.endswith("csv")]
-    cards: dict[str, Card] = {}
+    cards: dict[str, core.Card] = {}
     for file in files:
         cards.update(parse_gsheet_csv(file, set_tag))
     return cards
